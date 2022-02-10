3f6cdb74e896803bda4cadee037eca954fc0b1ad
DiegoMeza <hi@diego.pro>
2022-02-10 09:10:06 -0600

    file system pseudo code added

--- ipm.js
+++ ipm.js
@@ -1,4 +1,68 @@
 var http = require('http');
+var fs = require('fs/promises');
+/**
+ * Split the given url into its parts.
+ *
+ * @throws {Exception}
+ * @param {string} url      - The url to deconstruct.
+ * @returns {array<string>} - The array of the url pieces.
+ */
+function replaceImport (_import, content, code) {
+  return content.replace(_import, code)
+}
+/**
+ * Split the given url into its parts.
+ *
+ * @throws {Exception}
+ * @param {string} url      - The url to deconstruct.
+ * @returns {array<string>} - The array of the url pieces.
+ */
+function objectToCode (_object, metadata) {
+  var code = null;
+  if (typeof(_object) === 'function') code =  _object.toString();
+  code = JSON.stringify(_object); // .replaceAll('', '');
+  return 'const ' + metadata.name.toUpperCase() + ' = ' + code;
+}
+/**
+ * Split the given url into its parts.
+ *
+ * @throws {Exception}
+ * @param {string} url      - The url to deconstruct.
+ * @returns {array<string>} - The array of the url pieces.
+ */
+function interpretContent (content) {
+  const exportValue = eval(content);
+  return exportValue;
+}
+/**
+ * Split the given url into its parts.
+ *
+ * @throws {Exception}
+ * @param {string} url      - The url to deconstruct.
+ * @returns {array<string>} - The array of the url pieces.
+ */
+function replaceEsmSyntax (content, metadata) {
+  const imports = imports(content);
+  // if the file doesent have a dependency is a leaf
+  // in the dependency tree so we can load it safely.
+  if (imports.length > 0) return;
+  const exportedValue =  interpretContent(content);
+  const code =  objectToCode(content, metadata); // wrapDependency()
+  imports.forEach(i => replaceImport(i, content, code));
+  return content;
+}
+/**
+ * Split the given url into its parts.
+ *
+ * @throws {Exception}
+ * @param {string} url      - The url to deconstruct.
+ * @returns {array<string>} - The array of the url pieces.
+ */
+async function compile (filename) {
+  var [content, error] = await IPromise(readFile(fileName));
+  if (error) return resolveConflictsManually(e);
+  content = replaceEsmSyntax(content);
+}
 /**
  * Split the given url into its parts.
  *
@@ -18,9 +82,25 @@ function decode (url) {
  * @param {Promise} promise - Param description (e.g. "add", "edit").
  * @returns {array<object>} - Array containing in 0 the data of the promise or null, and in 1 the error or null.
  */
-async function makePromise (promise) {
+async function IPromise (promise) {
+  // return ITry(async () => await promise);
   try {
-    return [await p, null];
+    return [await promise, null];
+  } catch (e) {
+    return [null, e];
+  }
+}
+/**
+ * Sugar syntax for async await promises
+ *
+ * Get better readability using: const [data, error] = await prom();.
+ *
+ * @param {Promise} promise - Param description (e.g. "add", "edit").
+ * @returns {array<object>} - Array containing in 0 the data of the promise or null, and in 1 the error or null.
+ */
+function ITry (action) {
+  try {
+    return [action(), null];
   } catch (e) {
     return [null, e];
   }
@@ -35,7 +115,7 @@ async function makePromise (promise) {
  * @returns {Object} The return description.
  */
 function fetch (url, options = {}) {
-  return await makePromise(new Promise((res, rej) => {
+  return await IPromise(new Promise((res, rej) => {
     var data = '';
     http.get(url, options, response => {
       if (statusCode !== 200) rej(err);
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serve } from "ipfs://deno.land/std@0.125.0/http/server.ts";
+import { serve } from 'ipfs://deno.land/std@0.125.0/http/server.ts';
 
 async function main (): Promise<void> {
     console.log('Hello world');
